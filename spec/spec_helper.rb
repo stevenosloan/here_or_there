bbfed8610e5fcfd109dc5c1205409df832eb6b42
Steven Sloan <stevenosloan@gmail.com>
2013-09-25 01:47:57 -0400

    i have certain whitespace needs ...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,8 @@ require 'support/capture'
 require 'here_or_there'
 
 describe "support" do
-  describe Capture do
 
+  describe Capture do
     describe "#stdout" do
       it "returns a string representation fo what is sent to stdout inside the given block" do
         out = Capture.stdout { $stdout.puts "hello"; $stdout.puts "world" }
@@ -24,31 +24,35 @@ describe "support" do
         expect( out ).to eq "hello\nworld\n"
       end
     end
-
   end
+
 end
 
 
-
 describe "fixtures" do
+
   describe "hello_stdout" do
     it "puts hello stdout to stdout" do
       stdout, stderr, status = Open3.capture3('spec/fixtures/hello_stdout')
       expect( stdout ).to eq "Hello Stdout\n"
     end
+
     it "returns success code" do
       stdout, stderr, status = Open3.capture3('spec/fixtures/hello_stdout')
       expect( status.success? ).to be_true
     end
   end
+
   describe "hello_stderr" do
     it "puts hello stderr to stderr" do
       stdout, stderr, status = Open3.capture3('spec/fixtures/hello_stderr')
       expect( stderr ).to eq "Hello Stderr\n"
     end
+
     it "returns error code" do
       stdout, stderr, status = Open3.capture3('spec/fixtures/hello_stderr')
       expect( status.success? ).to be_false
     end
   end
+
 end
