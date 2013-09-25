ce24a985a79421159d531d5bdf01ad9948b31fbd
Steven Sloan <stevenosloan@gmail.com>
2013-09-25 02:18:05 -0400

    oh bolloks, this old stuff shouldn't have been included

--- lib/here_or_there/shell.rb
+++ lib/here_or_there/shell.rb
@@ -10,22 +10,5 @@ module HereOrThere
       return response
     end
 
-
-    def archive command, &block
-      stdout, stderr, status = Open3.capture3(command)
-
-      if status.success?
-        yield stdout if block_given?
-        return [ stdout, stderr, status ]
-      else
-        $stderr.puts "Problem running #{command}"
-        $stderr.puts stderr
-        return [ stdout, stderr, status ]
-      end
-    rescue StandardError => e
-      $stderr.puts "Problem running '#{command}'"
-      $stderr.puts "Error: #{e}"
-    end
-
   end
 end
